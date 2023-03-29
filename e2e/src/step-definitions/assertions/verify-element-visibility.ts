38855324f2ff6a545b4abe6e4d407eb2a9218708
jodial92 <jroberto.diaz@outlook.com>
2023-03-29 03:25:37 -0600

    Removing console.logs, adding mappings to feature steps

--- e2e/src/step-definitions/assertions/verify-element-visibility.ts
+++ e2e/src/step-definitions/assertions/verify-element-visibility.ts
@@ -5,14 +5,17 @@ import { getElementLocator } from '../../support/web-element-helper';
 
 Then(
     /^the "([^"]*)" should contain the text "(.*)"$/,
-    async function(elementKey: string, expectedElementText: string) {
+    async function(elementKey: ElementKey, expectedElementText: string) {
         const {
             screen: {page},
+            globalVariables,
+            globalConfig,
         } = this;
         
         console.log(`the ${elementKey} header should contain the text ${expectedElementText}`);
         
-        const content = await page.textContent("[data-id='contacts']");
+        const elementIdentifier = getElementLocator(page, elementKey, globalVariables, globalConfig);
+        const content = await page.textContent(elementIdentifier);
         
         expect(content).toBe(expectedElementText);
     }
